cc42da28dade2a07e6bf7dbbdb04d9f554ce632c
Alessandro Braga <ilbryan@users.noreply.github.com>
2020-10-07 15:59:52 +0200

    Update books.py

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -6,7 +6,7 @@ class BooksSpider(scrapy.Spider):
     name = "books"
     allowed_domains = ["thomsonlocal.com"]
     start_urls = [
-        'https://www.thomsonlocal.com/search/thai-therapy-massage/london',
+        'https://www.thomsonlocal.com/search/chinese-therapy/london',
     ]
 
     def parse(self, response):
