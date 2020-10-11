d995ee099ba94b1c4744dfdb9f4e6d3f32d0a779
Alessandro Braga <ilbryan@users.noreply.github.com>
2020-10-11 16:27:36 +0200

    Update books.py

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -20,8 +20,8 @@ class BooksSpider(scrapy.Spider):
         item = {}
         product = response.css("div.styles-module--wrapper--0a792c.compose-module--baseWrapper--ff8a8d")
         item["title"] = product.css("h1.Text-module_lgHeader__3Rm3T.style-module--name--9886df ::text").extract_first()
-        item["img"] = product.css("div.Carousel-module--wrapper--be7da7 > div > div > img").xpath('@src').getall()
+        item["img"] = product.css("div.Carousel-module--wrapper--be7da7 img.ResizingImage-module--image--23a8d7").xpath('@src').getall()
         item["where"] = product.css('.Text-module_xsHeader__1lwdU .style-module--addressPart--484b23 ::text').getall()
-        item["open"] = product.css('.styles-module--gridOpeningTimes--2ca6ce > .Stack-module_stack__1yFnE Stack-module_xs__3fXRh *::text').getall()
+        item["open"] = product.css('.styles-module--gridOpeningTimes--2ca6ce > .Stack-module_stack__1yFnE.Stack-module_xs__3fXRh *::text').getall()
         item['category'] = product.css('.Breadcrumbs-module--breadcrumbs--52ce45 *::text').getall()
         yield item
