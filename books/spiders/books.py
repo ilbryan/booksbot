945dff3dae08d2a994ae2f2460502f130df7d19f
Alessandro Braga <ilbryan@users.noreply.github.com>
2020-10-11 16:17:27 +0200

    Update books.py

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -4,30 +4,24 @@ import scrapy
 
 class BooksSpider(scrapy.Spider):
     name = "books"
-    allowed_domains = ["thomsonlocal.com"]
+    allowed_domains = ["treatwell.co.uk"]
     start_urls = [
-        'https://www.thomsonlocal.com/search/chinese-therapy/london',
+        'https://www.treatwell.co.uk/places/treatment-thai-massage/offer-type-local/in-london-uk/',
     ]
 
     def parse(self, response):
-        for book_url in response.css("li.listing > ul > .infoLink > div > a ::attr(href)").extract():
+        for book_url in response.css("div.Results-module--results--e3b89d > .BrowseResult-module--container--a0806d > div > a ::attr(href)").extract():
             yield scrapy.Request(response.urljoin(book_url), callback=self.parse_book_page)
-        next_page = response.css("li.next > a ::attr(href)").extract_first()
+        next_page = response.css("a.Pagination-module--next--740edf ::attr(href)").extract_first()
         if next_page:
             yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
 
     def parse_book_page(self, response):
         item = {}
-        product = response.css("div.branchDetails")
-        item["title"] = product.css("h1.listingName ::text").extract_first()
-        item["phone"] = product.css("li.phoneNumber > div > p ::text").extract_first()
-        item["web"] = product.css("a.webpageButton ::attr(href)").extract_first()
-        item["where"] = product.css('.address > span ::text').getall()
-        item['category'] = response.xpath(
-            "//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()"
-        ).extract_first()
-        item['description'] = response.xpath(
-            "//div[@id='product_description']/following-sibling::p/text()"
-        ).extract_first()
-        item['price'] = response.css('p.price_color ::text').extract_first()
+        product = response.css("div.styles-module--wrapper--0a792c.compose-module--baseWrapper--ff8a8d")
+        item["title"] = product.css("h1.Text-module_lgHeader__3Rm3T.style-module--name--9886df ::text").extract_first()
+        item["img"] = product.css("div.Carousel-module--wrapper--be7da7 > div > div > img").xpath('@src').getall()
+        item["where"] = product.css('.Text-module_xsHeader__1lwdU .style-module--addressPart--484b23 ::text').getall()
+        item["open"] = product.css('.styles-module--gridOpeningTimes--2ca6ce > .Stack-module_stack__1yFnE Stack-module_xs__3fXRh *::text').getall()
+        item['category'] = product.css('.Breadcrumbs-module--breadcrumbs--52ce45 *::text').getall()
         yield item
